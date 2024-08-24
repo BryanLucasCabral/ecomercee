ec8747263bf33b38f0d653ed1251ec5088ca2a39
BryanLucasCabral <bryan.brasil.bl@gmail.com>
2024-08-24 09:53:17 -0300

    Criado a entidade fornecedor

---
+++ src/main/java/Usuarios_Api/model/Fornecedor.java
@@ -0,0 +1,32 @@
+package Usuarios_Api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity(name = "tb_fornecedores")
+public class Fornecedor extends Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String razaoSocial;
+
+    @Column(nullable = false, unique = true, length = 14)
+    private String cnpj;
+
+    @Embedded
+    private Endereco endereco;
+}
